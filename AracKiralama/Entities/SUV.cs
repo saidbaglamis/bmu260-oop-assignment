9c1f4ecac76770aa91d1871b26757ca41cf348f2
Muhammed Said Baglamis <msbglms@gmail.com>
2025-05-30 14:13:40 +0300

    feat(suv): add Suv class

---
+++ AracKiralama/Entities/SUV.cs
@@ -0,0 +1,11 @@
+﻿namespace AracKiralama.Entities
+{
+    public class Suv : Arac
+    {
+        public override double KiraUcretiHesapla(int gun)
+        {
+            return GunlukUcret * gun * 1.10; // %10 fark
+        }
+    }
+
+}
