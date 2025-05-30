dfe3de47ec2d3dfa2347ba0aea4718cdfbb0ba23
Muhammed Said Baglamis <msbglms@gmail.com>
2025-05-30 14:16:20 +0300

    feat(musteri): add Musteri class

---
+++ AracKiralama/Entities/Musteri.cs
@@ -0,0 +1,12 @@
+﻿namespace AracKiralama.Entities
+{
+    public class Musteri
+    {
+        public int Id { get; set; }
+        public required string Ad { get; set; }
+        public required string Soyad { get; set; }
+        public required string TC { get; set; }
+        public required string Telefon { get; set; }
+    }
+
+}
--- AracKiralama/obj/Debug/net8.0-windows/AracKiralama.AssemblyInfo.cs
+++ AracKiralama/obj/Debug/net8.0-windows/AracKiralama.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AracKiralama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1f4ecac76770aa91d1871b26757ca41cf348f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c31d0597ad0484efea79d67ba60a407efa26fd6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AracKiralama")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AracKiralama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
